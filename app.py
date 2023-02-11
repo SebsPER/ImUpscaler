d6d648a48c620fdffd321415acf2444a137f1b35
SebsPER <67677649+SebsPER@users.noreply.github.com>
2023-02-11 13:23:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/upscale_image", methods=["POST"])
 def upscale_image():
     scale = int(request.args.get("scale", 2))
-    #image_binary = BytesIO(request.args.get("image").encode("utf-8"))
+    image_binary = BytesIO(request.args.get("image").encode("utf-8"))
     image = Image.open(request.args.get("image"))
     image = image.resize((image.width * scale, image.height * scale), Image.LANCZOS)
     result = BytesIO()
